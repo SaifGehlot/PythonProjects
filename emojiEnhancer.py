2392bdaf5e4facf2e7e72b045afe51f625ff66a8
Admin <admin@Admins-MacBook-Pro.local>
2026-09-11 06:15:02 -0700

    emoji enhancer done

---
+++ emojiEnhancer.py
@@ -0,0 +1,24 @@
+emojiMapFun = {
+  "love": "❤️",
+  "happy": "😀",
+  "code": "💻",
+  "chai": "☕️",
+  "music": "🎵",
+  "food": "🌭"
+}
+
+userMessage = input("Enter your message here: ").strip().lower()
+updatedWord = []
+
+for word in userMessage.split():
+  cleanedWord = word.strip(".,!?#$%^&*").lower()
+  emojiFound = emojiMapFun.get(cleanedWord, "")
+
+  if emojiFound:
+    updatedWord.append(f"{word} {emojiFound}")
+  else:
+    updatedWord.append(word)
+
+updatedMessage = " ".join(updatedWord)
+print("Enhanced Message: \n")
+print(updatedMessage)
